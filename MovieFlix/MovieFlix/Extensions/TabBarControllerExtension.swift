76a5ae98533eefa0a4097647f588c221738b3982
Jayd-parmar <aisehibanayahe64@gmail.com>
2023-10-30 10:02:41 +0530

    changes in navbar and tabbar appearances

--- MovieFlix/MovieFlix/Extensions/TabBarControllerExtension.swift
+++ MovieFlix/MovieFlix/Extensions/TabBarControllerExtension.swift
@@ -5,6 +5,17 @@
 //  Created by Jaydip Parmar on 27/10/23.
 //
 
-//import Foundation
-//
-//extension 
+import Foundation
+import UIKit
+
+extension UITabBarController {
+    
+    func tabBarAppearance() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.backgroundColor = UIColor.black
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        self.tabBar.backgroundColor = .black
+    }
+}
